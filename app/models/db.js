cefdcbcde4455eaeb8b5274208aa0e1e4dba3217
sajid <sajid@gitlab.com>
2016-11-06 20:03:11 +0000

    node launch script added

--- app/models/db.js
+++ app/models/db.js
@@ -1,30 +1,19 @@
 'use strict';
-
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+// let dbURI = 'mongodb://localhost/mytweet';
 
-let dbURI = 'mongodb://localhost/twitter';
+var dbURI = 'mongodb://mytweetuser:mytweetuser@ds145997.mlab.com:45997/mytwitter-web';
 if (process.env.NODE_ENV === 'production') {
   dbURI = process.env.MONGOLAB_URI;
 }
-
 mongoose.connect(dbURI);
-
-
 mongoose.connection.on('connected', function () {
   console.log('Mongoose connected to ' + dbURI);
-  if (process.env.NODE_ENV != 'production') {
-    var seeder = require('mongoose-seeder');
-    const data = require('./data.json');
-    const tweeter = require('./tweet');
-    const User = require('./user');
-    const Friend = require('./friend.js');
-    seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
-      console.log('preloading Test Data');
-      console.log(dbData);
-    }).catch(err => {
-      console.log(error);
-    });
-  }
 });
-
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
